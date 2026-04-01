cce8c9f64069121bb6ae79f48fbfefe92680b621
vishwajithmr11 <vishwajithmr9@gmail.com>
2026-04-01 20:34:09 +0530

    Add TodoItem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface TodoItemProps {
+  todo: {
+    id: string;
+    text: string;
+    completed: boolean;
+  };
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggle, onDelete }) => {
+  return (
+    <li style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => onToggle(todo.id)}
+      />
+      <span>{todo.text}</span>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default TodoItem;
