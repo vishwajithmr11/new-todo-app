856cdd20a3534bfcb21e00d8022cc2e834d07550
vishwajithmr11 <vishwajithmr9@gmail.com>
2026-04-01 20:34:26 +0530

    Add AddTodoForm component

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+interface AddTodoFormProps {
+  onAdd: (text: string) => void;
+}
+
+const AddTodoForm: React.FC<AddTodoFormProps> = ({ onAdd }) => {
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newTodo.trim() !== '') {
+      onAdd(newTodo);
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add new todo"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
